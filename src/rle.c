660105b8933cfd39bf868e80123e51f9db81d278
OmarcumarHasan <B00159813@mytudublin.ie>
2025-10-25 18:09:04 +0100

    Update rle.c
    used a copy of  my sample main c

---
+++ src/rle.c
@@ -0,0 +1,26 @@
+/*
+ * Title: RLE Compression Function
+ * Student ID: B00159813
+ * Name: Omar Hasan
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include "rle.h"
+
+// Performs Run-Length Encoding (RLE) compression and writes output into 'output' buffer
+// Returns the length of the compressed string
+int rle_compress(const char *input, char *output) {
+    int count = 1;
+    int j = 0;
+
+    for (int i = 0; i < strlen(input); i++) {
+        if (input[i] == input[i + 1]) {
+            count++;
+        } else {
+            j += sprintf(&output[j], "%c%d", input[i], count);
+            count = 1;
+        }
+    }
+    return j;
+}
