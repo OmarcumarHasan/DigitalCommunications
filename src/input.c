171cd3673fd42d8e650eb9c902cc203bb73c30cd
OmarcumarHasan <B00159813@mytudublin.ie>
2025-10-25 18:34:48 +0100

    Update input.c

---
+++ src/input.c
@@ -0,0 +1,30 @@
+/*
+ * Title: File Input Handler for RLE
+ * Student ID: B00159813
+ * Name: Omar Hasan
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "input.h"
+
+// Reads text input from a file into a buffer
+void read_file_input(char *buffer, const char *filename, int max_length) {
+    FILE *file = fopen(filename, "r");
+    // If fopen returns NULL, it means the file could not be opened (maybe it doesn’t exist)
+    if (file == NULL) {
+        printf("Error: Could not open file '%s'\n", filename);
+        exit(1);
+    }
+
+    int i = 0;
+    char c;
+    // Read each character one by one using fgetc()
+    // Keep reading until we reach End-Of-File (EOF)
+    // Also make sure we don’t write past the end of the buffer
+    while ((c = fgetc(file)) != EOF && i < max_length - 1) {
+        buffer[i++] = c;
+    }
+    buffer[i] = '\0'; // add a null terminator so that 'buffer' becomes a proper C string
+    fclose(file);
+}
