ee5bfd4fb9e2acf5ac001a064520b9e932f60af1
OmarcumarHasan <B00159813@mytudublin.ie>
2025-10-25 18:42:11 +0100

    made final analysis.c

---
+++ src/analysis.c
@@ -0,0 +1,38 @@
+/*
+ * Title: Data Analysis Functions for RLE
+ * Student ID: B00159813
+ * Name: Omar Hasan
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include "analysis.h"
+
+// Analyzes the data to find frequency, most/least frequent characters, and compression ratio
+void analyze_data(const char *input, const char *compressed) {
+    int freq[256] = {0};
+    int len_in = strlen(input);
+    int len_out = strlen(compressed);
+
+    // Count frequency of each character
+    for (int i = 0; i < len_in; i++) {
+        unsigned char c = input[i];
+        freq[c]++;
+    }
+
+    int max = 0, min = len_in;
+    unsigned char max_char = 0, min_char = 0;
+
+    for (int i = 0; i < 256; i++) {
+        if (freq[i] > 0) {
+            if (freq[i] > max) { max = freq[i]; max_char = i; }
+            if (freq[i] < min) { min = freq[i]; min_char = i; }
+        }
+    }
+
+    printf("\n--- Analysis ---\n");
+    printf("Most frequent character: '%c' (%d times)\n", max_char, max);
+    printf("Least frequent character: '%c' (%d times)\n", min_char, min);
+    printf("Compression ratio: %.2f\n", (float)len_in / len_out);
+    printf("-----------------\n");
+}
